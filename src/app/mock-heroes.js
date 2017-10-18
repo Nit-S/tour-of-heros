4f1abaca76cd665d0ab5d2c491dc4be65680d531
Nitigya sharma <nitigya.sharma12@gmail.com>
2017-10-18 14:05:28 +0530

    modulare service based

---
+++ src/app/mock-heroes.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var hero_1 = require("./hero");
+exports.HEROES = [
+    new hero_1.Hero(1, 'war machine'),
+    new hero_1.Hero(2, 'iron man'),
+    new hero_1.Hero(3, 'loda lussan'),
+    new hero_1.Hero(4, 'pudina ram'),
+    new hero_1.Hero(5, 'ravi kishan'),
+    new hero_1.Hero(6, 'mulchand sharma'),
+    new hero_1.Hero(7, 'dengu mal'),
+    new hero_1.Hero(8, 'gandu ram'),
+    new hero_1.Hero(9, 'chutiya'),
+    new hero_1.Hero(10, 'randi')
+];
+//# sourceMappingURL=mock-heroes.js.map
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export let HEROES: Hero[] = [
+    new Hero(1, 'war machine'),
+      new Hero(2, 'iron man'),
+       new Hero(3, 'loda lussan'),
+        new Hero(4, 'pudina ram'),
+         new Hero(5, 'ravi kishan'),
+          new Hero(6, 'mulchand sharma'),
+           new Hero(7, 'dengu mal'),
+            new Hero(8, 'gandu ram'),
+             new Hero(9, 'chutiya'),
+              new Hero(10, 'randi')
+               ];
+               
